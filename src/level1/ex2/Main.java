78a393bc8150e08b3a923778aae26c8e99292696
David Rey <tintoelculo@gmail.com>
2025-10-26 15:28:16 +0100

    feat: level1 ex3 and ex4 completed

--- src/level1/ex2/Main.java
+++ src/level1/ex2/Main.java
@@ -5,10 +5,11 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Ana", "Roberto", "Carolina", "Olivia", "Paco", "Sílvia", "Carmen", "Esteban", "Monica");
+        List<String> names = Arrays.asList("Ana", "Roberto", "Carolina", "Olivia", "Paco", "Sílvia", "Carmen",
+                "Esteban", "Monica");
         List<String> filteredNames = names.stream()
                 .filter(name -> name.contains("o"))
-                .filter(name -> name.length()>5)
+                .filter(name -> name.length() > 5)
                 .toList();
         filteredNames.forEach(System.out::println);
     }
